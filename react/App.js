ef14b77169f5532afd2dad716b9acb188b4e608d
jonatansalas <jonatansalas@live.com.ar>
2017-04-02 21:27:31 -0300

    Added logic to render items

--- react/App.js
+++ react/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Content } from 'native-base';
+import { TextInput } from 'react-native';
+import { Container, Content, List, ListItem, Text, CheckBox, Left } from 'native-base';
 
 import Header from './Header';
 import config from '../app.json';
@@ -12,12 +13,63 @@ export default class TodoApp extends Component {
     }
 
     render() {
+        const { state: { value, items } } = this;
+
         return (
             <Container>
                 <Header displayName={config.displayName}/>
-                <Content>
+                <Content style={{ padding: 8 }}>
+                    <TextInput
+                        value={value}
+                        placeholder='Insert a to do'
+                        onSubmitEditing={this.handleAdd}
+                        onChangeText={this.handleChangeText}
+                    />
+                    <List
+                        dataArray={items.reverse()}
+                        renderRow={this.renderItem}
+                    />
                 </Content>
             </Container>
         );
     }
+
+    handleChangeText = value => this.setState({ value });
+
+    handleAdd = () => {
+        let { items, value } = this.state;
+
+        items = [
+            ...items,
+            {
+                key: Date.now(),
+                text: value,
+                completed: false
+            }
+        ]
+
+        this.setState({ items, value: '' });
+    }
+
+    getPressHandler = item => {
+        return () => {
+            let { items } = this.state;
+
+            const index = items.findIndex(it => it.key === item.key);
+            items[index] = item;
+
+            this.setState({ items });
+        }
+    }
+
+    renderItem = ({ text, completed }) => (
+        <ListItem>
+            <Left>
+                <CheckBox checked={completed}/>
+            </Left>
+            <Text>
+                {text}
+            </Text>
+        </ListItem>
+    )
 }
