43267557c036c2c02998d353ca37068e3aca8a76
jonatansalas <jonatansalas@live.com.ar>
2017-04-03 23:00:25 -0300

    Treat TabBar text different depending on which Platform it runs

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,6 +3,8 @@ import { Platform } from 'react-native';
 import { Text, Footer, FooterTab, Button } from 'native-base';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+const capitalize = text => `${text.charAt(0).toUpperCase()}${text.slice(1).toLowerCase()}`;
+
 export default class TodoFooter extends Component {
     static propTypes = {
         changeFilter: PropTypes.func,
@@ -38,7 +40,7 @@ export default class TodoFooter extends Component {
         <FooterTab key={`tab-key-${index}`}>
             <Button onPress={() => this.props.changeFilter(tab.filter)}>
                 <Icon name={tab.icon} size={20} color={Platform.OS === 'ios' ? '#757575' : '#FFFFFF'}/>
-                <Text>{tab.filter}</Text>
+                <Text>{Platform.OS === 'ios' ? capitalize(tab.filter) : tab.filter.toUpperCase()}</Text>
             </Button>
         </FooterTab>
     )
